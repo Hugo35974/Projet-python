b2eea29c7b8485fc4f89ab1f11ec18e7becd5d30
TINOU\tinou <etienne.duclos@isen-ouest.yncrea.fr>
2024-01-24 14:49:25 +0100

    Push Main

--- Main.py
+++ Main.py
@@ -4,13 +4,11 @@ from pygame.locals import QUIT, MOUSEBUTTONDOWN
 from Interface import newBoard
 from Couleurs import *
 
-
 def initialize_game(width, height):
     pygame.init()
     win = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Chess Game")
     return win
-
 
 def draw_buttons(win, show_P1vsP2_button, show_IA_button):
     font = pygame.font.Font(None, 36)
@@ -21,21 +19,6 @@ def draw_buttons(win, show_P1vsP2_button, show_IA_button):
     play_text = font.render("Jouer", True, (0, 0, 0))
     win.blit(play_text, (
     play_button_rect.centerx - play_text.get_width() // 2, play_button_rect.centery - play_text.get_height() // 2))
-
-    # Sous-Boutons de "Jouer"
-    if show_P1vsP2_button :
-        P1vsP2_button_rect = pygame.Rect(50, 250, 200, 50)
-        pygame.draw.rect(win, (0, 255, 0), P1vsP2_button_rect)
-        play_text = font.render("Joueur vs Joueur", True, (0, 0, 0))
-        win.blit(play_text, (
-            P1vsP2_button_rect.centerx - play_text.get_width() // 2, P1vsP2_button_rect.centery - play_text.get_height() // 2))
-
-    if show_IA_button :
-        IA_button_rect = pygame.Rect(50, 250, 200, 50)
-        pygame.draw.rect(win, (0, 255, 0), IA_button_rect)
-        play_text = font.render("Joueur vs IA", True, (0, 0, 0))
-        win.blit(play_text, (
-            IA_button_rect.centerx - play_text.get_width() // 2, IA_button_rect.centery - play_text.get_height() // 2))
 
     # Bouton "Paramètre"
     settings_button_rect = pygame.Rect(50, 300, 200, 50)
@@ -52,6 +35,20 @@ def draw_buttons(win, show_P1vsP2_button, show_IA_button):
     quit_button_rect.centerx - quit_text.get_width() // 2, quit_button_rect.centery - quit_text.get_height() // 2))
 
     return play_button_rect, settings_button_rect, quit_button_rect
+
+def draw_sub_button(win):
+    # Sous-Boutons de "Jouer"
+    P1vsP2_button_rect = pygame.Rect(50, 250, 200, 50)
+    pygame.draw.rect(win, (0, 255, 0), P1vsP2_button_rect)
+    play_text = font.render("Joueur vs Joueur", True, (0, 0, 0))
+    win.blit(play_text, (
+        P1vsP2_button_rect.centerx - play_text.get_width() // 2, P1vsP2_button_rect.centery - play_text.get_height() // 2))
+
+    IA_button_rect = pygame.Rect(50, 250, 200, 50)
+    pygame.draw.rect(win, (0, 255, 0), IA_button_rect)
+    play_text = font.render("Joueur vs IA", True, (0, 0, 0))
+    win.blit(play_text, (
+        IA_button_rect.centerx - play_text.get_width() // 2, IA_button_rect.centery - play_text.get_height() // 2))
 
 
 if __name__ == "__main__":
@@ -75,6 +72,11 @@ if __name__ == "__main__":
                 mouse_pos = pygame.mouse.get_pos()
                 if play_button_rect.collidepoint(mouse_pos):
                     print("Jouer")
+                    play_button_rect = False
+                    settings_button_rect = False
+                    quit_button_rect = False
+                    show_P1vsP2_button = True
+                    show_IA_button = True
                 elif settings_button_rect.collidepoint(mouse_pos):
                     print("Paramètre")
                 elif quit_button_rect.collidepoint(mouse_pos):
@@ -85,6 +87,7 @@ if __name__ == "__main__":
         chess_board.draw_Board()
 
         # Dessiner les boutons
-        play_button_rect, settings_button_rect, quit_button_rect = draw_buttons(game_win,True, True)
+        play_button_rect, settings_button_rect, quit_button_rect = draw_buttons(game_win, False, False)
 
         pygame.display.flip()
+        pygame.display.update()
