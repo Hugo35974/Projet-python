bb22bdf94a81c72ef18c7614083f80daaf53086f
Hugo Bénard <>
2024-01-24 11:28:06 +0100

    Ajout de la regle

---
+++ regle.py
@@ -0,0 +1,14 @@
+class ReglesDuJeu:
+    def est_deplacement_valide(piece, depart, arrivee, grille):
+        """
+        Vérifie si un déplacement est valide selon les règles du jeu.
+
+        :param piece: Instance de la classe Piece.
+        :param depart: Tuple (ligne, colonne) représentant la position de départ.
+        :param arrivee: Tuple (ligne, colonne) représentant la position d'arrivée.
+        :param grille: Instance de la classe Grille représentant le plateau de jeu.
+        :return: True si le déplacement est valide, False sinon.
+        """
+        if piece and piece.deplacements_possibles(depart, grille):
+            return arrivee in piece.deplacements_possibles(depart, grille)
+        return False
